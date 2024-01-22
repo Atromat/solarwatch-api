9c8275f59cc2717141a34b779124ed22561f1861
Atromat <atromatok@gmail.com>
2024-01-22 19:06:38 +0100

    test: add test for SunsetSunriseJsonProcessor

---
+++ SolarWatch/SolarWatchTest/SunsetSunriseJsonProcessorTest.cs
@@ -0,0 +1,60 @@
+using SolarWatch.Services;
+
+namespace SolarWatchTest;
+
+public class SunsetSunriseJsonProcessorTest
+{
+    [Test]
+    public void GetSunsetReturnProperTimeOnly()
+    {
+        var processor = new SunsetSunriseJsonProcessor();
+
+        var timeOnly = processor.GetSunset(@$"{{
+            ""results"": {{
+                ""sunrise"": ""2024-01-22T04:53:43+00:00"",
+                ""sunset"": ""2024-01-22T15:29:06+00:00"",
+                ""solar_noon"": ""2024-01-22T10:11:24+00:00"",
+                ""day_length"": 38123,
+                ""civil_twilight_begin"": ""2024-01-22T04:29:31+00:00"",
+                ""civil_twilight_end"": ""2024-01-22T15:53:17+00:00"",
+                ""nautical_twilight_begin"": ""2024-01-22T04:00:26+00:00"",
+                ""nautical_twilight_end"": ""2024-01-22T16:22:23+00:00"",
+                ""astronomical_twilight_begin"": ""2024-01-22T03:31:48+00:00"",
+                ""astronomical_twilight_end"": ""2024-01-22T16:51:00+00:00""
+            }},
+            ""status"": ""OK"",
+            ""tzid"": ""UTC""
+        }}");
+        
+        Console.WriteLine($"timeonly: {timeOnly.Hour}:{timeOnly.Minute}:{timeOnly.Second}");
+        
+        Assert.That(timeOnly.Hour == 15 && timeOnly.Minute == 29 && timeOnly.Second == 6);
+    }
+    
+    [Test]
+    public void GetSunriseReturnProperTimeOnly()
+    {
+        var processor = new SunsetSunriseJsonProcessor();
+
+        var timeOnly = processor.GetSunrise(@$"{{
+            ""results"": {{
+                ""sunrise"": ""2024-01-22T04:53:43+00:00"",
+                ""sunset"": ""2024-01-22T15:29:06+00:00"",
+                ""solar_noon"": ""2024-01-22T10:11:24+00:00"",
+                ""day_length"": 38123,
+                ""civil_twilight_begin"": ""2024-01-22T04:29:31+00:00"",
+                ""civil_twilight_end"": ""2024-01-22T15:53:17+00:00"",
+                ""nautical_twilight_begin"": ""2024-01-22T04:00:26+00:00"",
+                ""nautical_twilight_end"": ""2024-01-22T16:22:23+00:00"",
+                ""astronomical_twilight_begin"": ""2024-01-22T03:31:48+00:00"",
+                ""astronomical_twilight_end"": ""2024-01-22T16:51:00+00:00""
+            }},
+            ""status"": ""OK"",
+            ""tzid"": ""UTC""
+        }}");
+        
+        Console.WriteLine($"timeonly: {timeOnly.Hour}:{timeOnly.Minute}:{timeOnly.Second}");
+        
+        Assert.That(timeOnly.Hour == 4 && timeOnly.Minute == 53 && timeOnly.Second == 43);
+    }
+}
