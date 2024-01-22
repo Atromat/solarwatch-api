0c62d8caf3e4881602124ad817ed11068a942caf
Atromat <atromatok@gmail.com>
2024-01-22 19:02:34 +0100

    feat: add controller for getting sunset and sunrise time

---
+++ SolarWatch/SolarWatch/Controllers/SolarWatchController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using SolarWatch.Services;
+
+namespace SolarWatch.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SolarWatchController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IJsonProcessor _jsonProcessor;
+    private readonly ICoordDataProvider _coordDataProvider;
+    private readonly ISunsetSunriseDataProvider _sunsetSunriseDataProvider;
+    private readonly ISunsetSunriseJsonProcessor _sunsetSunriseJsonProcessor;
+
+    public SolarWatchController(ILogger<WeatherForecastController> logger, IJsonProcessor jsonProcessor, 
+        ICoordDataProvider coordDataProvider, ISunsetSunriseDataProvider sunsetSunriseDataProvider,
+        ISunsetSunriseJsonProcessor sunsetSunriseJsonProcessor)
+    {
+        _logger = logger;
+        _jsonProcessor = jsonProcessor;
+        _coordDataProvider = coordDataProvider;
+        _sunsetSunriseDataProvider = sunsetSunriseDataProvider;
+        _sunsetSunriseJsonProcessor = sunsetSunriseJsonProcessor;
+    }
+
+    [HttpGet("GetSunriseTime")]
+    public ActionResult<TimeOnly> GetSunriseTime(string cityName)
+    {
+        try
+        {
+            var cityData = _coordDataProvider.GetDataByCity(cityName);
+            var cityLatLon = _jsonProcessor.GetLongitudeLatitude(cityData);
+            var sunriseData = _sunsetSunriseDataProvider.GetDataByLongitudeLatitude(cityLatLon.Item1, cityLatLon.Item2);
+            var sunriseTime = _sunsetSunriseJsonProcessor.GetSunrise(sunriseData);
+            return Ok(sunriseTime);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting sun rise data");
+            return NotFound("Error getting sun rise data");
+        }
+    }
+    
+    [HttpGet("GetSunsetTime")]
+    public ActionResult<TimeOnly> GetSunsetTime(string cityName)
+    {
+        try
+        {
+            var cityData = _coordDataProvider.GetDataByCity(cityName);
+            var cityLatLon = _jsonProcessor.GetLongitudeLatitude(cityData);
+            var sunsetData = _sunsetSunriseDataProvider.GetDataByLongitudeLatitude(cityLatLon.Item1, cityLatLon.Item2);
+            var sunsetTime = _sunsetSunriseJsonProcessor.GetSunset(sunsetData);
+            return Ok(sunsetTime);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting sun rise data");
+            return NotFound("Error getting sun rise data");
+        }
+    }
+}
--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,3 +1,5 @@
+using SolarWatch.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ICoordDataProvider, OpenWeatherMapApi>();
+builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+builder.Services.AddSingleton<ISunsetSunriseDataProvider, SunriseSunsetApi>();
+builder.Services.AddSingleton<ISunsetSunriseJsonProcessor, SunsetSunriseJsonProcessor>();
 
 var app = builder.Build();
 
