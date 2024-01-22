1d13d11c36d10460665b184c36413c9650bbc922
Atromat <atromatok@gmail.com>
2024-01-22 18:06:45 +0100

    feat: add json processor for getting longitude and latitude from https://api.openweathermap.org response string

---
+++ SolarWatch/SolarWatch/Services/JsonProcessor.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+
+namespace SolarWatch.Services;
+
+public class JsonProcessor : IJsonProcessor
+{
+    public (double, double) GetLongitudeLatitude(string openWeatherMapApiAnswer)
+    {
+        JsonDocument json = JsonDocument.Parse(openWeatherMapApiAnswer);
+        var firstJsonElement = json.RootElement.EnumerateArray().ToArray().First();
+        var latitude = firstJsonElement.GetProperty("lat").GetDouble();
+        var longitude = firstJsonElement.GetProperty("lon").GetDouble();
+        
+        Console.WriteLine(latitude);
+        Console.WriteLine(longitude);
+
+        return (latitude, longitude);
+    }
+}
