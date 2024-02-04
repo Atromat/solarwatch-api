9da197e234545fe955e066d0860e427735e4636a
Atromat <atromatok@gmail.com>
2024-02-04 22:06:18 +0100

    refactor: SolarWatchController to have async methods

--- SolarWatch/SolarWatch/Controllers/SolarWatchController.cs
+++ SolarWatch/SolarWatch/Controllers/SolarWatchController.cs
@@ -25,11 +25,11 @@ public class SolarWatchController : ControllerBase
     }
 
     [HttpGet("GetSunriseTime")]
-    public ActionResult<TimeOnly> GetSunriseTime(string cityName)
+    public async Task<ActionResult<TimeOnly>> GetSunriseTime(string cityName)
     {
         try
         {
-            var cityData = _coordDataProvider.GetDataByCity(cityName);
+            var cityData = await _coordDataProvider.GetDataByCity(cityName);
             var cityLatLon = _jsonProcessor.GetLongitudeLatitude(cityData);
             var sunriseData = _sunsetSunriseDataProvider.GetDataByLongitudeLatitude(cityLatLon.Item1, cityLatLon.Item2);
             var sunriseTime = _sunsetSunriseJsonProcessor.GetSunrise(sunriseData);
@@ -43,11 +43,11 @@ public class SolarWatchController : ControllerBase
     }
     
     [HttpGet("GetSunsetTime")]
-    public ActionResult<TimeOnly> GetSunsetTime(string cityName)
+    public async Task<ActionResult<TimeOnly>> GetSunsetTime(string cityName)
     {
         try
         {
-            var cityData = _coordDataProvider.GetDataByCity(cityName);
+            var cityData = await _coordDataProvider.GetDataByCity(cityName);
             var cityLatLon = _jsonProcessor.GetLongitudeLatitude(cityData);
             var sunsetData = _sunsetSunriseDataProvider.GetDataByLongitudeLatitude(cityLatLon.Item1, cityLatLon.Item2);
             var sunsetTime = _sunsetSunriseJsonProcessor.GetSunset(sunsetData);
