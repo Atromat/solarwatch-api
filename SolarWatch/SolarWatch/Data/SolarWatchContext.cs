d79afcf6990dd98e58d08871a00db2fdb3ba5f24
Atromat <atromatok@gmail.com>
2024-02-05 07:00:48 +0100

    feat: add SolarWatchContext (a DbContext)

---
+++ SolarWatch/SolarWatch/Data/SolarWatchContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using SolarWatch.Model;
+
+namespace SolarWatch.Data;
+
+public class SolarWatchContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunriseSunset> SunriseSunsets { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=SolarWatchApi;User Id=sa;Password=hgUrjkl8in12;Encrypt=false;");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Configure the City entity - making the 'Name' unique
+        builder.Entity<City>()
+            .HasIndex(u => u.Name)
+            .IsUnique();
+    
+        builder.Entity<City>()
+            .HasData(
+                new City { Id = 1, Name = "London", Latitude = 51.509865, Longitude = -0.118092, State = null, Country = "England"},
+                new City { Id = 2, Name = "Budapest", Latitude = 47.497913, Longitude = 19.040236, State = null, Country = "Hungary" },
+                new City { Id = 3, Name = "Paris", Latitude = 48.864716, Longitude = 2.349014, State = null, Country = "France"}
+            );
+    }
+}
