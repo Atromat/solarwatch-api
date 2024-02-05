330f044e4a20b316b509a7565fdc808bb6acdc9e
Atromat <atromatok@gmail.com>
2024-02-05 08:35:45 +0100

    fix: add missing methods to ISunsetSunriseJsonProcessor

--- SolarWatch/SolarWatch/Services/ISunsetSunriseJsonProcessor.cs
+++ SolarWatch/SolarWatch/Services/ISunsetSunriseJsonProcessor.cs
@@ -4,4 +4,10 @@ public interface ISunsetSunriseJsonProcessor
 {
     TimeOnly GetSunrise(string sunsetSunriseData);
     TimeOnly GetSunset(string sunsetSunriseData);
+
+    public DateTime GetSunriseDateTime(string sunsetSunriseData);
+
+    public DateTime GetSunsetDateTime(string sunsetSunriseData);
+
+    public int GetDayLength(string sunriseSunsetData);
 }
