b3a7d0628716fe113b1a0e64dc77ca7ad5702897
Atromat <atromatok@gmail.com>
2024-02-18 21:28:31 +0100

    fix: add missing method and change Delete param in ISunriseSunsetRepository

--- SolarWatch/SolarWatch/Services/Repositories/ISunriseSunsetRepository.cs
+++ SolarWatch/SolarWatch/Services/Repositories/ISunriseSunsetRepository.cs
@@ -5,10 +5,11 @@ namespace SolarWatch.Services.Repositories;
 public interface ISunriseSunsetRepository
 {
     IEnumerable<SunriseSunset> GetAll();
+    public SunriseSunset? GetById(int id);
     SunriseSunset? GetByName(string cityName);
     SunriseSunset? GetByNameAndDate(string cityName, DateTime dateTime);
 
     void Add(SunriseSunset sunriseSunset);
-    void Delete(SunriseSunset sunriseSunset);
+    void Delete(int id);
     void Update(SunriseSunset sunriseSunset);
 }
