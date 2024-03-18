2e61f69a2039db48711914fd8e48a00d6caa15ee
Atromat <atromatok@gmail.com>
2024-03-18 20:48:56 +0100

    test: add registration to the tests where needed

--- SolarWatch/SolarWatchIntegrationTests/SolarWatchControllerTests.cs
+++ SolarWatch/SolarWatchIntegrationTests/SolarWatchControllerTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using SolarWatch.Contracts;
+using SolarWatch.Model;
 
 namespace SolarWatchIntegrationTests;
 
@@ -21,12 +22,14 @@ public class SolarWatchControllerTests : IClassFixture<WebApplicationFactory<Pro
     public async Task GetSunriseSunsetReturnsOk()
     {
         //Arrange
-        var request = new AuthRequest("sim@gmail.com", "simsim");
+        var registerRequest = new RegistrationRequest("sim@gmail.com", "sim", "simsim");
+        await _client.PostAsJsonAsync("Auth/Register", registerRequest);
+        var loginRequest = new AuthRequest("sim@gmail.com", "simsim");
+        await _client.PostAsJsonAsync("Auth/Login", loginRequest);
         
         //Act
-        await _client.PostAsJsonAsync("Auth/Login", request);
         var response = await _client.GetAsync("SolarWatch/GetSunriseSunset?cityName=Delhi&year=2021&month=3&day=3");
-        await _client.PostAsJsonAsync("Auth/Logout", request);
+        await _client.PostAsJsonAsync("Auth/Logout", loginRequest);
         
         //Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -48,23 +51,21 @@ public class SolarWatchControllerTests : IClassFixture<WebApplicationFactory<Pro
     public async Task GetSunriseSunsetReturnsProperValuesForInput()
     {
         //Arrange
-        var request = new AuthRequest("sim@gmail.com", "simsim");
+        var registerRequest = new RegistrationRequest("sim@gmail.com", "sim", "simsim");
+        await _client.PostAsJsonAsync("Auth/Register", registerRequest);
+        var loginRequest = new AuthRequest("sim@gmail.com", "simsim");
+        await _client.PostAsJsonAsync("Auth/Login", loginRequest);
         
         //Act
-        await _client.PostAsJsonAsync("Auth/Login", request);
         var response = await _client.GetAsync("SolarWatch/GetSunriseSunset?cityName=Delhi&year=2019&month=9&day=9");
-        await _client.PostAsJsonAsync("Auth/Logout", request);
+        await _client.PostAsJsonAsync("Auth/Logout", loginRequest);
         
         //Assert
-        JsonDocument json = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-        var sunsetId = json.RootElement.GetProperty("id").ToString();
-        var sunrise = json.RootElement.GetProperty("sunrise").ToString();
-        var sunset = json.RootElement.GetProperty("sunset").ToString();
-        var dayLength = json.RootElement.GetProperty("dayLength").ToString();
+        var sunriseSunset = await response.Content.ReadFromJsonAsync<SunriseSunset>();
+        
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        Assert.Equal(22.ToString(), sunsetId);
-        Assert.Equal("2019-09-09T00:32:58", sunrise);
-        Assert.Equal("2019-09-09T13:06:00", sunset);
-        Assert.Equal(45182.ToString(), dayLength);
+        Assert.Equal(new DateTime(2019, 09, 09, 0, 32, 58), sunriseSunset.Sunrise);
+        Assert.Equal(new DateTime(2019, 09, 09, 13, 6, 0), sunriseSunset.Sunset);
+        Assert.Equal(45182, sunriseSunset.DayLength);
     }
 }
