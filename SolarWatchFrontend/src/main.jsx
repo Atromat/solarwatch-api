6b3ef97cacff0f6c312558c5ac7539462d03cfe6
Atromat <atromatok@gmail.com>
2024-03-21 00:49:59 +0100

    refactor: move url and createBrowserRouter from main.jsx to App.jsx

--- SolarWatchFrontend/src/main.jsx
+++ SolarWatchFrontend/src/main.jsx
@@ -1,56 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-import Home from './Pages/Home.jsx';
-import Layout from './Pages/Layout/Layout.jsx';
-import ErrorPage from './Pages/ErrorPage.jsx';
-import CityTable from './Pages/CityTable.jsx';
-import SunsetSunriseTable from './Pages/SunsetSunriseTable.jsx';
-import Login from './Pages/Login.jsx';
-import Register from './Pages/Register.jsx';
-import SunsetSunrise from './Pages/SunsetSunrise.jsx';
-
-const url = "http://localhost:5062/";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/SunsetSunrise",
-        element: <SunsetSunrise />,
-      },
-      {
-        path: "/SunsetSunriseTable",
-        element: <SunsetSunriseTable />,
-      },
-      {
-        path: "/CityTable",
-        element: <CityTable />,
-      },
-      {
-        path: "/Register",
-        element: <Register url={url} />,
-      },
-      {
-        path: "/Login",
-        element: <Login url={url} />,
-      }
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
